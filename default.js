96203e9d6c34ff64cc029b54b7823fa2d6d931a5
Mathemagiks <mathemagiks@gmail.com>
2019-05-29 10:08:11 +0100

    Create default.js

---
+++ default.js
@@ -0,0 +1,43 @@
+{[ "Mon": {
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "17",
+"defaultEndTimeMinute" : "30"
+},
+"Tue": {
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "17",
+"defaultEndTimeMinute" : "30"
+},
+"Wed":{
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "17",
+"defaultEndTimeMinute" : "30"
+},
+"Thur":{
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "17",
+"defaultEndTimeMinute" : "30"
+},
+"Fri":{
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "14",
+"defaultEndTimeMinute" : "30"
+},
+"Sat": {
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "17",
+"defaultEndTimeMinute" : "30"
+},
+"Sun":{
+"defaultStartTimeHour" : "09",
+"defaultStartTimeMinute" : "00",
+"defaultEndTimeHour" : "17",
+"defaultEndTimeMinute" : "30"
+}]
+}
