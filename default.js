852cc4c2490e04445dac1bcefdf2aa35690657ee
Mathemagiks <mathemagiks@gmail.com>
2019-05-29 10:55:25 +0100

    Update default.js

--- default.js
+++ default.js
@@ -1,43 +1,59 @@
-[{ "Mon": {
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "17",
-"defaultEndTimeMinute" : "30"
-},
-"Tue": {
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "17",
-"defaultEndTimeMinute" : "30"
-},
-"Wed":{
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "17",
-"defaultEndTimeMinute" : "30"
-},
-"Thur":{
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "17",
-"defaultEndTimeMinute" : "30"
-},
-"Fri":{
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "14",
-"defaultEndTimeMinute" : "30"
-},
-"Sat": {
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "17",
-"defaultEndTimeMinute" : "30"
-},
-"Sun":{
-"defaultStartTimeHour" : "09",
-"defaultStartTimeMinute" : "00",
-"defaultEndTimeHour" : "17",
-"defaultEndTimeMinute" : "30"
-}
+[{
+	"defaultTimes": [{
+			"Mon": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "17",
+				"defaultEndTimeMinute": "30"
+			}
+		},
+		{
+			"Tue": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "17",
+				"defaultEndTimeMinute": "30"
+			}
+		},
+		{
+			"Wed": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "17",
+				"defaultEndTimeMinute": "30"
+			}
+		},
+		{
+			"Thur": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "17",
+				"defaultEndTimeMinute": "30"
+			}
+		},
+		{
+			"Fri": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "14",
+				"defaultEndTimeMinute": "30"
+			}
+		},
+		{
+			"Sat": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "17",
+				"defaultEndTimeMinute": "30"
+			}
+		},
+		{
+			"Sun": {
+				"defaultStartTimeHour": "09",
+				"defaultStartTimeMinute": "00",
+				"defaultEndTimeHour": "17",
+				"defaultEndTimeMinute": "30"
+			}
+		}
+	]
 }]
